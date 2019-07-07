233e0dbc79631b69e9ce236413c783d6ac79c51a
新增 gitignore <>
2019-07-07 17:17:31 +0800

    rghhh

--- Class02/ViewController.swift
+++ Class02/ViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var result : float_t = 10.2
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //我要去超市了
+        
+        let 如果我去超市 = true
+        
+        //有橘子
+        let 如果看到橘子 = true
+        
+        if 如果我去超市 {
+            var 买西瓜的数量 = 1
+            
+            
+            if 如果看到橘子 {
+                买西瓜的数量 = 10
+            }
+            
+            
+            
+            print("我要买：\(买西瓜的数量)个西瓜")
+        }
+}
+
+    func add(a : Int, b : Int) -> Int {
+        return a + b
     }
-
-
 }
 
