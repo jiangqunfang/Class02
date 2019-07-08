fe1b01b976bdd0972618d0f0fb728e666c94b0b9
新增 gitignore <>
2019-07-08 17:12:18 +0800

    0708

--- Class02/ViewController.swift
+++ Class02/ViewController.swift
@@ -9,10 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var lbMsg: UILabel!
     
     var result : float_t = 10.2
     
-
+    @IBAction func btnTestCllicked(_ sender: UIButton)
+    {
+        print("我按了啥?")
+        
+        lbMsg.text = "我要草莓"
+        lbMsg.textColor = UIColor.red
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -25,10 +33,8 @@ class ViewController: UIViewController {
         
         if 如果我去超市 {
             var 买西瓜的数量 = 1
-            
-            
-            
-            
+             
+        
             if 如果看到橘子 {
                 买西瓜的数量 = 10
             }
